517771e0586c1326c8b267799fefd1c5f7f8da88
HappyFaceFriend <yjl980730@gmail.com>
2022-10-19 17:55:40 +0900

    sorting layer 수정 및 gameloop 임시 적용

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,15 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : SingletonBehaviour<LevelManager>
+public class LevelManager : MonoBehaviour
 {
 
     [SerializeField] Flower _flower;
     [SerializeField] List<FlowerPlantPoint> _dirts;
 
+    [SerializeField] SlimeSpawner _spawner;
+
     private void Start()
+    {
+        StartCoroutine(GameLoop());
+    }
+    
+    void InitFlower()
     {
         int dirtIdx = Random.Range(0, _dirts.Count);
         _dirts[dirtIdx].PlantFlower(_flower);
+    }
+    IEnumerator GameLoop()
+    {
+        //Init
+        InitFlower();
+        _spawner.Init();
+        //Loop
+        while (_spawner)
+        {
+            _spawner.StartNextStage();
+            yield return _spawner.WaitUntilStageClear();
+
+            //업그레이드
+
+        }
+
+
+    }
+
+    public void OnPlayerDead()
+    {
+
     }
 }
