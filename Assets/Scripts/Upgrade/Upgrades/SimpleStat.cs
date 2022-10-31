0a7386bd026c3320bfba2fa8e57153c4f81512c4
jaeby99 <qo0102@naver.com>
2022-10-31 18:45:56 +0900

    업그레이드 수정 및 1종 추가

--- Assets/Scripts/Upgrade/Upgrades/SimpleStat.cs
+++ Assets/Scripts/Upgrade/Upgrades/SimpleStat.cs
@@ -7,28 +7,43 @@ namespace Upgrades
     [CreateAssetMenu(menuName = "Upgrade Datas/Simple Stat")]
     public class SimpleStat : UpgradeData
     {
-        public enum StatType { MaxHp, AttackPower, AttackSpeed, MoveSpeed, PushToTowerRange }
-
-        [SerializeField] int _numberOfStats;
-        [SerializeField] StatType _statToChange;
-        [SerializeField] float _applyValue;
-        [SerializeField] Modifier.ApplyType _applyType;
-        [SerializeField] 
+        [System.Serializable]
+        public class StatMode
+        {
+            public enum StatType { MaxHp, CurrentHP, AttackPower, AttackSpeed, MoveSpeed, PushToTowerRange }
+            public StatType _statToChange;
+            public float _applyValue;
+            public Modifier.ApplyType _applyType;
+        }
+       
+        [SerializeField] List<StatMode> _statModes;
 
         public override void OnAdded()
         {
             base.OnAdded();
-            Modifier modifier = new Modifier(_applyValue, _applyType);
-            if (_statToChange == StatType.MaxHp)
-                GlobalRefs.Player.MaxHp.AddModifier(modifier);
-            else if (_statToChange == StatType.AttackPower)
-                GlobalRefs.Player.AttackPower.AddModifier(modifier);
-            else if (_statToChange == StatType.AttackSpeed)
-                GlobalRefs.Player.AttackSpeed.AddModifier(modifier);
-            else if (_statToChange == StatType.MoveSpeed)
-                GlobalRefs.Player.MoveSpeed.AddModifier(modifier);
-            else if (_statToChange == StatType.PushToTowerRange)
-                GlobalRefs.Player.PushToTowerRange.AddModifier(modifier);
+            GenerateChange(_statModes);
         }
+
+        public void GenerateChange(List<StatMode> modes)
+        {
+            for (int i = 0; i < modes.Count; i++)
+            {
+                var stat = modes[i];
+                Modifier modifier = new Modifier(stat._applyValue, stat._applyType);
+                if (stat._statToChange == StatMode.StatType.MaxHp)
+                    GlobalRefs.Player.MaxHp.AddModifier(modifier);
+                else if (stat._statToChange == StatMode.StatType.CurrentHP)
+                    GlobalRefs.Player.HpSystem.ChangeHp(stat._applyValue);
+                else if (stat._statToChange == StatMode.StatType.AttackPower)
+                    GlobalRefs.Player.AttackPower.AddModifier(modifier);
+                else if (stat._statToChange == StatMode.StatType.AttackSpeed)
+                    GlobalRefs.Player.AttackSpeed.AddModifier(modifier);
+                else if (stat._statToChange == StatMode.StatType.MoveSpeed)
+                    GlobalRefs.Player.MoveSpeed.AddModifier(modifier);
+                else if (stat._statToChange == StatMode.StatType.PushToTowerRange)
+                    GlobalRefs.Player.PushToTowerRange.AddModifier(modifier);
+            }
+        }
+        
     }
 }
