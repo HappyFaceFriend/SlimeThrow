07c28bd6e4e55d307b983b65e7a7afbf37971e62
HappyFaceFriend <yjl980730@gmail.com>
2022-12-05 19:25:28 +0900

    끈끈이 발자국 효과 prefab 추가

---
+++ Assets/Scripts/Effects/StickyFootStepGenerator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyFootStepGenerator : MonoBehaviour
+{
+    [SerializeField] float interval;
+    [SerializeField] Vector3 randomOffset;
+    float eTime = 0;
+
+    private void OnEnable()
+    {
+        eTime = 0;
+    }
+    private void Update()
+    {
+        eTime += Time.deltaTime;
+        if(eTime >= interval)
+        {
+            eTime -= interval;
+            EffectManager.InstantiateStickyFootstep(transform.position + Utils.Random.RandomVector(-randomOffset/2, randomOffset/2));
+        }
+    }
+}
