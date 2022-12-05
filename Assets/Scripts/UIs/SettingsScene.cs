3b58dee6408dc8842de9b2fc41a576f24d89b6ca
HappyFaceFriend <yjl980730@gmail.com>
2022-12-05 20:42:07 +0900

    설정창 프로토타입 완성

---
+++ Assets/Scripts/UIs/SettingsScene.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsScene : MonoBehaviour
+{
+    [SerializeField] Scrollbar volumeSlider;
+    [SerializeField] Image koButtonPressed;
+    [SerializeField] Image koButtonNotPressed;
+    [SerializeField] Image enButtonPressed;
+    [SerializeField] Image enButtonNotPressed;
+
+    private void Start()
+    {
+        volumeSlider.value = SaveDataManager.Instance.Volume;
+        SetLanguageAs(SaveDataManager.Instance.Language);
+    }
+    public void OnVolumeSet()
+    {
+        SaveDataManager.Instance.Volume = volumeSlider.value;
+    }
+    public void OnLanguageButtonPressed(string code)
+    {
+        SetLanguageAs(code);
+        SaveDataManager.Instance.Language = code;
+    }
+    
+    void SetLanguageAs(string code)
+    {
+        if (code == "ko")
+        {
+            koButtonPressed.gameObject.SetActive(true);
+            koButtonNotPressed.gameObject.SetActive(false);
+            enButtonPressed.gameObject.SetActive(false);
+            enButtonNotPressed.gameObject.SetActive(true);
+        }
+        else
+        {
+            koButtonPressed.gameObject.SetActive(false);
+            koButtonNotPressed.gameObject.SetActive(true);
+            enButtonPressed.gameObject.SetActive(true);
+            enButtonNotPressed.gameObject.SetActive(false);
+        }
+    }
+}
